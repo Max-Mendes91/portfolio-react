d48fb2a9c99068f56482ce4826969fb1b8503cb6
Max-Mendes91 <maxmendesnoah1991@gmail.com>
2025-10-26 10:16:32 +0100

    Enhance header button: Added functionality to open contact form and updated button styling for improved appearance.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -148,8 +148,10 @@ const Header = () => {
                     <button
                         onClick={() => {
                             toggleMenu()
+                            openContactForm()
                         }}
-                        className='mt-4 block w-full px-4 py-2 row-lg bg-gradient-to-r from via-violet-600 to-violet-400 font-bold'>
+                        className='mt-4 block w-full px-4 py-2 rounded-lg bg-gradient-to-r from-violet-600 via-violet-500 to-violet-400 font-bold'
+                        >
                         Contact Me
                     </button>
                 </div>
